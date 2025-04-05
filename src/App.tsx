757d1a8a1162fae5c0aba0c96eeae8ccd07f1dff
Gina Wang <14085239+gqycloud@users.noreply.github.com>
2025-04-04 19:50:32 -0700

    lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-    // App.jsx
-    import React from 'react'
+// App.jsx
+import React from "react";
 
-    function App() {
-      return (
+function App() {
+    return (
         <div>
-          <h1>Hello, React!</h1>
+            <h1>Hello, React!</h1>
         </div>
-      )
-    }
+    );
+}
 
-    export default App
+export default App;
