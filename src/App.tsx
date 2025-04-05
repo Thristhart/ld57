e7a1cff97200bf90c500d9a452c8685eec89395e
Gina Wang <14085239+gqycloud@users.noreply.github.com>
2025-04-04 20:15:32 -0700

    stub in some UI pieces

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import { DepthMeter, Fuel, HullIntegrity, Inventory, LightSwitch, UpgradeGUI } from "./ui/components";
 
 function App() {
     return (
         <div className={"App"}>
-            <div className={"LeftUI"}>GINAS UI PART</div>
+            <div className={"LeftUI"}>
+                <DepthMeter />
+                <HullIntegrity />
+                <Fuel />
+                <LightSwitch />
+            </div>
             <div className={"Center"}>TOM YOU CAN HAVE THIS PART</div>
-            <div className={"RightUI"}>GINAS OTHER UI PART</div>
+            <div className={"RightUI"}>
+                <Inventory />
+                <UpgradeGUI />
+            </div>
         </div>
     );
 }
